858ca6b8f4fbdb29211a53f5162bdd5a44416d6b
Danilo-de-Carvalho <danilodecarvalho88@gmail.com>
2023-07-27 19:26:42 -0300

    Concerto dos erros do ex011

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70;
+console.log(`A velocidade de seu carro é de ${vel}Km/h`);
+//Condição simples
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida! MUTADO!`);
+}
+console.log(`Dirija sempre utilizando cinto de segurança!`);
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Canada';
+console.log(`Você esta vivendo no(a) ${pais}`);
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro');
+} else {
+    console.log('Você é estrangeiro');
+}
